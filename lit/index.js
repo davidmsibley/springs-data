f8503f0410fbe7c3024382e3c2431c7a320b6e10
David M Sibley <sibley@wisc.edu>
2019-06-17 17:43:07 -0500

    Updates

--- lit/index.js
+++ lit/index.js
@@ -567,6 +567,7 @@
       ${(!this.siteinfo)? '' : litElement.html`
         <h1 class="header">${this.siteinfo.County} County Spring #${this.siteinfo.SpringID}</h1>
         <site-photos .photos="${this.photos}"></site-photos>
+        <slot name="sketch"></slot>
         <site-bed-materials .siteinfo="${this.siteinfo}"></site-bed-materials> 
         <div data-element="table">
           ${Object.entries(this.siteinfo).map((el, index) => litElement.html`
@@ -584,8 +585,99 @@
   }
   customElements.define('site-details', SiteDetails);
 
+  let pdfjsLib = window['pdfjs-dist/build/pdf'];
+
+  class SiteSketch extends litElement.LitElement {
+    static get properties() {
+      return {
+        pdfsrc: {
+          type: String
+        },
+        imgsrc: {
+          type: String
+        }
+      };
+    }
+
+    buildImgSrc() {
+      if (this.pdfsrc) {
+        let renderRoot = this.renderRoot;
+        let cleanedUrl = this.pdfsrc.replace('http://data.wgnhs.uwex.edu/', 'https://data.wgnhs.wisc.edu/');
+        // console.log(this.pdfsrc, cleanedUrl);
+        let canvasEl = document.createElement('canvas');
+        let loadingTask = pdfjsLib.getDocument(cleanedUrl);
+        return loadingTask.promise.then(function(pdf) {
+          // console.log('PDF Loaded');
+          var pageNumber = 1;
+          return pdf.getPage(pageNumber).then(function(page) {
+            // console.log('Page loaded');
+            
+            var scale = 1.0;
+            var viewport = page.getViewport({scale: scale});
+
+            // Prepare canvas using PDF page dimensions
+            var canvas = canvasEl;
+            var context = canvas.getContext('2d');
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+
+            // Render PDF page into canvas context
+            var renderContext = {
+              canvasContext: context,
+              viewport: viewport
+            };
+            var renderTask = page.render(renderContext);
+            return renderTask.promise.then(function () {
+              // console.log('Page rendered');
+              let durl = canvasEl.toDataURL();
+              return durl;
+            });
+          });
+        }, function (reason) {
+          console.error(reason);
+        });
+      }
+      return Promise.resolve(null);
+    }
+
+    constructor() {
+      super();
+    }
+
+    static get styles() {
+      return litElement.css`
+
+    `;
+    }
+
+    render() {
+      return litElement.html`
+    <div>
+      ${(!this.imgsrc)?'':litElement.html`
+      <img src="${this.imgsrc}" />
+      `}
+    </div>
+    `;
+    }
+
+    updated(old) {
+      var el = this;
+      if (old.has('pdfsrc')) {
+        if (el.pdfsrc) {
+          el.buildImgSrc().then(function(url) {
+            el.imgsrc = url;
+          });
+        } else {
+          el.imgsrc = null;
+        }
+      }
+    }
+  }
+  customElements.define('site-sketch', SiteSketch);
+
   exports.AppSidebar = AppSidebar;
   exports.SiteDetails = SiteDetails;
+  exports.SiteSketch = SiteSketch;
 
   Object.defineProperty(exports, '__esModule', { value: true });
 
